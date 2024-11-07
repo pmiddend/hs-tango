2179f16fe77546165c6890c4980ad144608ada1f
Philipp Middendorf <philipp.middendorf@desy.de>
2024-11-07 10:56:56 +0100

    🔖 Increase version and remove stray file

--- run-chef.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# cabal run -- chef --beamtime-id 116 --amarcord-url http://cfeld-vm04:6020 --p11-runner-identifier 'tango://cfeld-tango:10000/dev_tapedrive/p11/runner'
-
-cabal run -- chef --beamtime-id 119 --amarcord-url http://localhost:5001 --p11-runner-identifier 'tango://cfeld-tango:10000/dev_tapedrive/p11/runner'
